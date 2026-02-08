530fe6ca60221fdaef66c2cfb54e9e45422ee4c0
Chris Demos <irademos@gmail.com>
2026-02-07 23:53:57 -0500

    sword tweak

--- items/autumnSword.js
+++ items/autumnSword.js
@@ -8,6 +8,7 @@ export class AutumnSword extends Weapon {
       type: 'sword',
       modelUrl: '/assets/props/autumn_sword.glb',
       scale: 2.0,
+      holdOffset: new THREE.Vector3(0.75, 0.0, 0.0),
       fallbackSize: new THREE.Vector3(0.1, 0.8, 0.2),
       fallbackColor: 0xe2b14b
     });
